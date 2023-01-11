03569f7aa218b8badf574a526dee166a457c01f5
Rana Arouk <ranaarrouk@gmail.com>
2023-01-11 21:33:22 +0200

    Edit welcome & home views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+    <div class="container">
+        <div class="row">
+            @foreach($blogs as $blog)
+                <div class="col-6 col-md-4">
+                    <div class="card  mb-3">
+                        <a href="{{ route("blogs.show", $blog->id) }}" style=" text-decoration: inherit;color: inherit;">
+                            <div class="card-header">{{ $blog->title }}
+                            </div>
+                            <div class="card-body">
+                                <img width="100%" src="{{ asset('storage/blogs/images/' . $blog->image) }}"
+                                     class="img-thumbnail">
+                                <p class="card-text"><small class="text-muted">{{ $blog->publish_date }}</small></p>
+                                <p class="card-text"><small class="text-muted">{{ \Illuminate\Support\Str::limit($blog->content, 100) }}</small></p>
+                            </div>
+                        </a>
+                    </div>
                 </div>
-            </div>
+            @endforeach
         </div>
     </div>
-</div>
 @endsection
